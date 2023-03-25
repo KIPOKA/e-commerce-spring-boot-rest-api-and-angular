69313c9661575b39479735ff2c7a826868edb483
220825213 <220825213@tut4life.ac.za>
2023-03-25 22:20:17 +0200

    rest api updated and working on angular

--- 03-frontend/ecommerce-angular/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/ecommerce-angular/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/service/product.service';
 
@@ -9,12 +10,27 @@ import { ProductService } from 'src/app/service/product.service';
 })
 export class ProductListComponent implements OnInit{
   products: Product[]=[];
-  constructor(private productService: ProductService){}
+  currentCategoryId: number=1;
+  constructor(private productService: ProductService, private route:ActivatedRoute){}
   ngOnInit(): void { 
-    this.listProducts();
+    this.route.paramMap.subscribe(()=>{
+      this.listProducts();
+    });
+    
   }
   listProducts() {
-    this.productService.getProductList().subscribe(
+    //check if the id parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if(hasCategoryId){
+      //get the 'id' string. convert the string to a number using the "+" symbol
+      this.currentCategoryId  = +this.route.snapshot.paramMap.get('id')!;
+    }else{
+      //If the category is not available 
+      this.currentCategoryId =1;
+    }
+
+    //Now we need to get the product category for the given id of the same category
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data =>{
         this.products = data;
       }
